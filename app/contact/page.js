fa6f4c531724264ea3943100be1979d6ed86bc3d
billy740 <b.hassan200313@gmail.com>
2025-03-19 15:02:06 +0300

    added contact page

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,1 +1,125 @@
-c
+import Breadcrumb from "@/components/Breadcrumb";
+import NextLayout from "@/layouts/NextLayout";
+const page = () => {
+  return (
+    <NextLayout>
+      <Breadcrumb pageName="Contact Us" />
+      {/* Contact Section Section Start */}
+      <section className="contact-section section-padding">
+        <div className="container">
+          <div className="contact-wrapper">
+            <div className="row g-4">
+              <div className="col-lg-6">
+                <div className="contact-content">
+                  <div className="section-title">
+                    <span className="sub-content wow fadeInUp">
+                      <img src="assets/img/bale.png" alt="img" />
+                      Contact Us
+                    </span>
+                    <h2 className="wow fadeInUp" data-wow-delay=".3s">
+                      Don’t Hessite to Contact <br />
+                      Our Team Member
+                    </h2>
+                  </div>
+                  <p className="mt-3 mt-md-0 wow fadeInUp" data-wow-delay=".5s">
+                    Sed ut perspiciatis unde omnis iste natus error voluptatem
+                    accusantium <br />
+                    doloremque laudantium, totam rem aperiam
+                  </p>
+                  <ul
+                    className="contact-list wow fadeInUp"
+                    data-wow-delay=".3s"
+                  >
+                    <li>
+                      <a href="partnership@swahilipothub.co.ke">partnership@swahilipothub.co.ke</a>
+                    </li>
+                    <li>Mombasa, Kenya</li>
+                    <li>
+                      <a href="tel:+254 11 4635505">+254 11 4635505</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="col-lg-6">
+                <div
+                  className="contact-right wow fadeInUp"
+                  data-wow-delay=".4s"
+                >
+                  <h3>Send Us Message</h3>
+                  <form
+                    action="#"
+                    id="contact-form"
+                    method="POST"
+                    className="contact-form-items"
+                  >
+                    <div className="row g-4">
+                      <div className="col-lg-6">
+                        <div className="form-clt">
+                          <input
+                            type="text"
+                            name="name"
+                            id="name"
+                            placeholder="Full Name"
+                          />
+                        </div>
+                      </div>
+                      <div className="col-lg-6">
+                        <div className="form-clt">
+                          <input
+                            type="text"
+                            name="phone"
+                            id="phone"
+                            placeholder="Phone"
+                          />
+                        </div>
+                      </div>
+                      <div className="col-lg-12">
+                        <div className="form-clt">
+                          <input
+                            type="text"
+                            name="email"
+                            id="email2"
+                            placeholder="Your Email"
+                          />
+                        </div>
+                      </div>
+                      <div className="col-lg-12">
+                        <div className="form-clt">
+                          <textarea
+                            name="message"
+                            id="message"
+                            placeholder="Comments"
+                            defaultValue={""}
+                          />
+                        </div>
+                      </div>
+                      <div className="col-lg-6">
+                        <button type="submit" className="theme-btn">
+                          Send a Message
+                        </button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Map Section Start */}
+      <div className="map-section">
+        <div className="map-items">
+          <div className="googpemap">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3979.7893833802746!2d39.67731977474537!3d-4.063306044967009!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1840131ba0153b67%3A0x75a36ee817c3f38b!2sSwahilipot%20Hub!5e0!3m2!1sen!2ske!4v1718785438427!5m2!1sen!2ske"
+              style={{ border: 0 }}
+              allowFullScreen=""
+              loading="lazy"
+            />
+          </div>
+        </div>
+      </div>
+    </NextLayout>
+  );
+};
+export default page;
